e95acffe6c8a619e83bd11a99205e33cfdbe090e
CJRemley29 <cremley29@gmail.com>
2023-09-28 11:59:34 -0400

    Update HomePage.js
    Added tabs to the homepage NOT FULL FUNCTIONALITY

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -3,6 +3,8 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import MovieCard from './MovieCard';
 import Form from 'react-bootstrap/Form';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function HomePage () {
@@ -11,15 +13,27 @@ function HomePage () {
             <Form style={{margin: '1rem'}}>
             <Form.Control type="search" placeholder="search for a movie" />
             </Form>
-                <Row sm={1} md={2} lg={3} xl={4} className="g-4" bg={"dark"}>
-                    {Array.from({ length: 5 }).map((_, idx) => (
-                    <Col key={idx} >
-                        <MovieCard/>
-                    </Col>
-                    ))}
-                </Row>
+
+            <Tabs
+                defaultActiveKey="Curently Playing"
+                fill
+                justify
+                data-bs-theme="dark"
+            >
+                <Tab eventKey="Curently Playing" title="Curently Playing">
+                    <Row sm={1} md={2} lg={3} xl={4} className="g-4" bg={"dark"}>
+                        {Array.from({ length: 5 }).map((_, idx) => (
+                        <Col key={idx} >
+                            <MovieCard/>
+                        </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="Coming Soon" title="Coming Soon">
+                    <MovieCard/>
+                </Tab>
+            </Tabs>
         </Container>
-        
       );
 
 }
