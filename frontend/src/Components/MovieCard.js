c06fa958f366c1ccd71d04859919cf05b7aac2ed
arnabbiswas0 <043.biswasarnab@gmail.com>
2023-10-01 14:30:33 -0400

    Results in error

--- frontend/src/Components/MovieCard.js
+++ frontend/src/Components/MovieCard.js
@@ -7,9 +7,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MovieCard({title, poster, trailer}) {
 
-  const [showTrailer, setShowTrailer] = useState(false);
+  const [showTrailer, setShowTrailer, BookMovie, setBookMovie] = useState(false);
   const handleCloseTrailer = () => setShowTrailer(false);
   const handleShowTrailer = () => setShowTrailer(true);
+  const handleCloseBookMovie = () => setBookMovie(false);
+  const handleShowBookMovie = () => setBookMovie(true);
+
 
   return (
     <> 
@@ -21,8 +24,8 @@ function MovieCard({title, poster, trailer}) {
       <Card.Img variant="top" src={poster} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
-        <Button style={{margin: '0.5rem'}}variant="primary" onClick={handleShowTrailer}>watch trailer</Button>
-        <Button variant="primary" onClick={handleShowTrailer}>Book Movie</Button>
+        <Button style={{margin: '0.5rem'}}variant="primary" onClick={handleShowTrailer}>View trailer</Button>
+        <Button variant="primary" onClick={handleShowBookMovie}>Book Movie</Button>
         {(localStorage.getItem("Admin")==="Admin") &&
             <Button variant="primary" onClick={handleShowTrailer}>Edit Movie</Button>
         }
@@ -50,8 +53,37 @@ function MovieCard({title, poster, trailer}) {
             </Button>
         </Modal.Footer>
     </Modal>
+
+    <Modal
+        show={BookMovie} 
+        onHide={handleCloseBookMovie}
+        centered
+        size={'lg'}
+        backdrop="static"
+    >
+        <Modal.Header closeButton>
+            <Modal.Title>Booking:</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Ratio aspectRatio="16x9">
+                <iframe class="embed-responsive-item" src={trailer}></iframe>
+            </Ratio>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="primary" onClick={handleCloseBookMovie}>
+                Close
+            </Button>
+        </Modal.Footer>
+    </Modal>
     </>
+
+
+
+
+
+
     );
+           
 }
 
 export default MovieCard;
