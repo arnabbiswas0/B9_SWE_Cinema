8aad56d0b8c11aeebb1250b87aa9378e23a04198
arnabbiswas0 <043.biswasarnab@gmail.com>
2023-10-01 19:13:03 -0400

    Added MovieData
    Untested if functional

---
+++ frontend/src/Components/movieData.js
@@ -0,0 +1,69 @@
+import Movie from './models/Movie';
+
+export const movies = [
+    new Movie(
+        "Star Wars",
+        "PG-13",
+        15,
+        "https://www.movieposters.com/cdn/shop/products/6cd691e19fffbe57b353cb120deaeb8f_8489d7bf-24ba-4848-9d0f-11f20cb35025_480x.progressive.jpg?v=1573613877",
+        "https://www.youtube.com/embed/8Qn_spdM5Zg?si=NfOSKmfV8oY-MzMh",
+        true
+    ),
+    new Movie(
+        "The Matrix",
+        "Rated R",
+        15,
+        "https://www.themoviedb.org/t/p/original/Cv6uwxudNU6kDdGwgOJNNxCEWI.jpg",
+        "https://www.youtube.com/embed/vKQi3bBA1y8?si=TUyatw3RI1dCMLyC",
+        true
+    ),
+    new Movie(
+        "The Dark Knight",
+        "Rated PG-13",
+        15,
+        "https://www.themoviedb.org/t/p/original/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+        "https://www.youtube.com/embed/_PZpmTj1Q8Q?si=yYQBWFGW791sdnIm",
+        true
+    ),
+    new Movie(
+        "The Shawshank Redemption",
+        "Rated R",
+        15,
+        "https://th.bing.com/th/id/OIP.vUgDFGJI9UpJl-xe-V0FOwHaLH?pid=ImgDet&rs=1",
+        "https://www.youtube.com/embed/NmzuHjWmXOc?si=6iwD6Zfz6MbKV5aq" ,
+        true
+    ),
+    new Movie(
+        "Pulp Fiction",
+        "Rated R",
+        15,
+        "https://th.bing.com/th/id/OIP.z6bKjfyEmcYCRRPy4b0_KgHaLH?pid=ImgDet&rs=1",
+        "https://www.youtube.com/embed/s7EdQ4FqbhY?si=ghEbDi4qUruVqeWP",
+        true
+    ),
+    new Movie(
+        "The Godfather",
+        "Rated R",
+        15,
+        "https://th.bing.com/th/id/OIP.EZncYnNoDxdf2bBsnDbQigAAAA?pid=ImgDet&rs=1",
+        "https://www.youtube.com/embed/UaVTIH8mujA?si=OAAwlaMid_CZZAsp" ,
+        true
+    ),
+    new Movie(
+        "Fight Club",
+        "Rated R",
+        15,
+        "https://th.bing.com/th/id/R.df8bdd4062afaf7eb535aaf0263f7696?rik=AJCKleaJVMnpag&pid=ImgRaw&r=0",
+        "https://www.youtube.com/embed/BdJKm16Co6M?si=3GrTzQcR7cpVtH9V",
+        true
+    ),
+    new Movie(
+        "The Dark Knight Rises",
+        "Rated PG-13",
+        15,
+        "https://th.bing.com/th/id/R.131839f1e962ad28e2b7b8a8437d35a8?rik=VYnCWrekPNCYLA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-Vx303suuu5g%2fTvybAi9HpRI%2fAAAAAAAACEc%2fXImDAPUbVZY%2fs1600%2fbatman%2bthe%2bdark%2bknight%2brises-girzl.blogspot.com-dark-knight-rises-fan-art2.jpeg&ehk=%2f387syviyBUtjIzsJiCIjgz8oNiXfLnyLh0yLHJdgDo%3d&risl=&pid=ImgRaw&r=0",
+        "https://www.youtube.com/embed/g8evyE9TuYk?si=DzT1Vl4UAMnxciyP",
+        true
+    ),
+
+];
