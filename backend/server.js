79d7d769a231f3770b8f97fa66142388acaff3d2
arnabbiswas0 <91227955+arnabbiswas0@users.noreply.github.com>
2023-10-05 11:20:05 -0400

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 6001;
+const { Schema } = mongoose;
+const router = express.Router();
+const { ObjectId } = require('mongodb');
+const routes = require("./routes") // new
+
+
+
+var cors = require('cors')
+
+const app = express();
+app.use(cors())
+
+
+const mysql = require('mysql2');
+
+/*Connect to database*/
+const CONNECTION_STRING = `mongodb+srv://arnab:test@swe-project.ulhiyxf.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.use(express.json())
+    app.listen(port);
+    app.use("/api", routes)
+    console.log('Mongo DB connection successful');
+  })
+  .catch((error) => {
+    console.log(`Error in DB connection:  ${error}`);
+  });
+
+
+let server = app.listen(8000, function () {
+    console.log('Server is listening at port 8000...');
+});
+
+
+
+
+
+
+
