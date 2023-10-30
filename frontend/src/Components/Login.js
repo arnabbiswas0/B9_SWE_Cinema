16a813d6d55888a5ded4fc3f5d2bc73f9bd104bd
CJRemley29 <cremley29@gmail.com>
2023-10-30 13:22:41 -0400

    added frontend functionality to login

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,9 +1,9 @@
 import '../Styles/SignUp.css';
 import React, { useState } from "react"
+import { useLogin } from "./hooks/useLogin";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
 import { Container, Form,} from 'react-bootstrap';
  
 
@@ -12,7 +12,19 @@ function Login() {
   const [showConfirm, setShowConfirm] = useState(false);
   const handleCloseConfirm = () => setShowConfirm(false);
   const handleShowConfirm = () => setShowConfirm(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const {login, error, isLoading} = useLogin();
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+
+    await login(email, password);
+    
+    handleShowConfirm();
+
+  }
 
     return (
     <>
@@ -28,17 +40,17 @@ function Login() {
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className='light-text'>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} value={email} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label className='light-text'>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
       </Form.Group>
       <Button style={{margin: '0.5rem'}} variant="primary"  onClick={handleShowConfirm}>
         Forgot Password
       </Button>
-      <Button variant="primary"  onClick={handleShowConfirm}>
+      <Button variant="primary"  onClick={handleSubmit} disabled={isLoading}>
         Submit
       </Button>
     </Form>
