8107612644ff7d26f78aee1c1d5813b0916a9c6a
arnabbiswas0 <043.biswasarnab@gmail.com>
2023-11-03 14:09:01 -0400

    init

--- backend/routes.js
+++ backend/routes.js
@@ -9,17 +9,17 @@ const bcrypt = require('bcrypt')
 const validator = require('validator')
 const nodemailer = require('nodemailer');
 const connection = mysql.createConnection({
-        host: process.env.HOST,
-        password: process.env.PASSWORD,
-        user: process.env.USER,  
+        host: 'arnabbiswas1.ddns.net',
+        password: 'Remote-password',
+        user: 'remote_user',  
         database: 'cinemasystem',
         port:3306
       });
 
 const promisequery = promisesql.createConnection({
-        host: process.env.HOST,
-        password: process.env.PASSWORD,
-        user: process.env.USER,  
+        host: 'arnabbiswas1.ddns.net',
+        password: 'Remote-password',
+        user: 'remote_user',  
         database: 'cinemasystem',
         port:3306
       });
