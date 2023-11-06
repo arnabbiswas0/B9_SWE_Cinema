79624dd46fa24a3a05dd48a6ee6835d017c3978e
NolbertChastain <79341405+NolbertChastain@users.noreply.github.com>
2023-11-06 12:46:49 -0500

    testing remote push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import HomePage from './Components/HomePage';
 import NavBar from './Components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+// simple remote push test
 function App() {
   return (
     <div className="App">
